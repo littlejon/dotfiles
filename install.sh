dc2eabd73a1f1b067070798687520acc832b2575
Littlejon <smithj@servercore.io>
2023-08-19 11:40:07 +1000

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,12 @@
 # Pull down git submodules
 git -C ~/dotfiles submodule update --init --recursive
 
+export DEBIAN_FRONTEND=noninteractive
+
+sudo apt-get update -y
+sudo apt-get -y install --no-install-recommends zsh
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
 # Overwrite zsh config
 ln -svf ~/dotfiles/.zshrc ~/.zshrc
-
